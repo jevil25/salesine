6ff07dfd58cb0e06ec2845030bf12ac3dacc7709
Parth Puranik <thosparth@gmail.com>
2023-06-22 12:24:14 +0530

    Chatbox frontend

---
+++ src/components/Message.jsx
@@ -0,0 +1,13 @@
+export default function Message(){
+    return(
+        <div>
+        <div id="messageInfo" style={{display:"flex"}}>
+          <p id="messageAuthor">Parth - </p>
+          <p id="timestamp" style={{color:"grey"}}> 10:12 AM,Today</p>
+        </div>
+        <div style={{ height: "40px", backgroundColor: "#87bb72",width:"450px" }}>
+          I like what Jim had to say about the startegy to vring in the new goods
+        </div>
+      </div>
+    )
+}
--- src/pages/recordings.js
+++ src/pages/recordings.js
@@ -1,64 +1,93 @@
-import { Divider, Card, Button, Group, Badge } from '@mantine/core';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import Navbar from '../components/Navbar';
-import ReactPlayer from 'react-player';
+import { Divider, Card, Button, Group, Badge } from "@mantine/core";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import ReactPlayer from "react-player";
+import Message from "../components/Message";
 
 export default function Recordings() {
-  const [topic, setTopic] = useState('');
-  const [id, setId] = useState('');
-  const [recording_drive_link, setRecording_drive_link] = useState('');
-  const [trans, setTrans] = useState('');
-  const router = useRouter()
-  if (typeof window !== 'undefined') {
-    if(localStorage.getItem('token') === null) {
-      router.push('/login');
+  const [topic, setTopic] = useState("");
+  const [id, setId] = useState("");
+  const [recording_drive_link, setRecording_drive_link] = useState("");
+  const [trans, setTrans] = useState("");
+  const router = useRouter();
+  if (typeof window !== "undefined") {
+    if (localStorage.getItem("token") === null) {
+      router.push("/login");
     }
   }
-  const [btn, setBtn] = useState('Transcript');
+  const [btn, setBtn] = useState("Transcript");
   const toggleHandler = () => {
-    if (btn === 'Recording') {
-      setBtn('Transcript');
+    if (btn === "Recording") {
+      setBtn("Transcript");
     } else {
-      setBtn('Recording');
-    } 
-  }
+      setBtn("Recording");
+    }
+  };
 
   useEffect(() => {
-    const data = JSON.parse(localStorage.getItem('recording'));
+    const data = JSON.parse(localStorage.getItem("recording"));
     console.log(data);
     setTopic(data.topic);
     setId(data.id);
     setRecording_drive_link(data.recording_drive_link);
-    setTrans(data.trans)
-    console.log(topic)
+    setTrans(data.trans);
+    console.log(topic);
   }, [typeof window]);
 
   return (
     <>
-    <Navbar type = 'recording' />
-      <div style={{margin: '20px'}}>
+      <Navbar type="recording" />
+      <div style={{ margin: "20px" }}>
         <h1>Recording for {topic}</h1>
-          <h2>ID : {id} </h2>
+        <h2>ID : {id} </h2>
         <Divider />
-        <br/>
-        <div style={{display: 'flex',  alignItems: 'center'}}>
-          { btn === 'Transcript' ?
-            <iframe src={`https://drive.google.com/file/d/${recording_drive_link}/preview`} width="740" height="400" allow='autoplay'></iframe>
-            : 
-            <Card shadow="sm" padding="xs"  radius="md"   withBorder style={{height: '400px', cursor: 'pointer', width: '60%' }}>
-              <h2 style={{textAlign: 'center'}}>Transcript</h2>
-              <h3>{trans}</h3>
-            </Card>
-          }
-          <Card shadow="sm" padding="xs"  radius="md"   withBorder style={{height: '400px', cursor: 'pointer', marginLeft: '30px', width: '30%' }}>
-            <h2 style={{textAlign: 'center'}}>Chat Window</h2>
-            
-          </Card>
+        <br />
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr",
+            columnGap: "40px",
+          }}
+        >
+          <div style={{ display: "flex", alignItems: "center" }}>
+            {btn === "Transcript" ? (
+              <iframe
+                src={`https://drive.google.com/file/d/${recording_drive_link}/preview`}
+                width="740"
+                height="400"
+                allow="autoplay"
+              ></iframe>
+            ) : (
+              <Card
+                shadow="sm"
+                padding="xs"
+                radius="md"
+                withBorder
+                style={{ height: "400px", cursor: "pointer", width: "60%" }}
+              >
+                <h2 style={{ textAlign: "center" }}>Transcript</h2>
+                <h3>{trans}</h3>
+              </Card>
+            )}
+          </div>
+          <div id="chatbox">
+            <div id="messages" style={{ height: "300px", overflowY: "auto" }}>
+              <Message />
+              <Message />
+              <Message />
+              <Message />
+              <Message />
+            </div>
+            <div className="messageInput" style={{marginTop:"10px"}}>
+              <input type="text" placeholder="Enter a message" />
+              <button>Send</button>
+            </div>
+          </div>
         </div>
-        <br/>
+        <br />
         <Group>
-          <Button size="md" onClick={() => router.push('/')}>
+          <Button size="md" onClick={() => router.push("/")}>
             Go Back
           </Button>
           <Button size="md" variant="outline" onClick={toggleHandler}>
