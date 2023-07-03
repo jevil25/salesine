ae145a4d7d59b30f4038e22d45a1db12ce6f1042
AaronNazareth <4nm20cs005@nmamit.in>
2023-07-03 19:09:38 +0530

    fix:google video

--- src/google/video.js
+++ src/google/video.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import axios from 'axios';
 
 
@@ -6,19 +6,24 @@ function Video({ id }) {
     console.log(id);
     const [video, setVideo] = useState(null);
     const [accessToken, setAccessToken] = useState("");
+    const [fetchVideo, setFetchVideo] = useState(true);
     const BACK_END_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:4000" ;
-    fetch(`${BACK_END_URL}/googleAccessToken`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            },
-        body: JSON.stringify({
-            email: process.env.NEXT_PUBLIC_EMAIL,
-        })
-    }).then((res) => res.json()).then((data) => {
-        setAccessToken(data.googleAccessToken);
-        getVideo();
-    });
+
+    function getAccessToken() {
+        fetch(`${BACK_END_URL}/googleAccessToken`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                },
+            body: JSON.stringify({
+                email: process.env.NEXT_PUBLIC_EMAIL,
+            })
+        }).then((res) => res.json()).then((data) => {
+            setAccessToken(data.googleAccessToken);
+            console.log(data.googleAccessToken);
+            getVideo();
+        });
+    }
     function getVideo(){
     console.log("Fetching video from drive");
       axios({
@@ -34,11 +39,15 @@ function Video({ id }) {
         const blob = new Blob([response.data], { type: 'video/mp4' });
         const videoURL = window.URL.createObjectURL(blob);
         setVideo(videoURL);
+        setFetchVideo(false);
         }).catch((error) => {
             console.log(error);
       }
     );
-}
+    }
+    if(fetchVideo){
+        getAccessToken();
+    }
     //play the video using iframe
     return (
         <div>
