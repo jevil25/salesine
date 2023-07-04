71251bf4cb4675c3ca65f9599225c4e349160cec
Parth Puranik <thosparth@gmail.com>
2023-07-04 10:39:21 +0530

    user flow

--- src/pages/advance_register.js
+++ src/pages/advance_register.js
@@ -1,134 +1,153 @@
 import {
-    TextInput,
-    PasswordInput,
-    Checkbox,
-    Anchor,
-    Paper,
-    Title,
-    Text,
-    Container,
-    Group,
-    Button,
-  } from '@mantine/core';
-  import Link from 'next/link';
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router'
+  TextInput,
+  PasswordInput,
+  Checkbox,
+  Anchor,
+  Paper,
+  Title,
+  Text,
+  Container,
+  Group,
+  Button,
+} from "@mantine/core";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
-export default function advance_register(){
-    const BACK_END_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:4000" ;
-    const [oldpassword,setoldpass] = useState("");
-    const [newpassword,setnewpass] = useState("");
-    const [email,setEmail] = useState("")
-    useEffect(()=>{
-        const email = localStorage.getItem("email")
-        setEmail(email)
-    },[])
+export default function advance_register() {
+  const router = useRouter();
+  const { id } = router.query;
+  console.log(id)
+  if(id==1){
+    let voicediv = document.getElementById("voiceregistration");
+    voicediv.style.display =  "none"
+  }
+  else if(id==2){
+    let passdiv = document.getElementById("passwordchange");
+    passdiv.style.display = "none"
+  }
+  const BACK_END_URL =
+    process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:4000";
+  const [oldpassword, setoldpass] = useState("");
+  const [newpassword, setnewpass] = useState("");
+  const [email, setEmail] = useState("");
+  useEffect(() => {
+    const email = localStorage.getItem("email");
+    setEmail(email);
 
-    async function changepassword(){
-        await fetch(`${BACK_END_URL}/changepassword`,{
-            method:'POST',
-            headers:{
-                'Content-Type':'application/json'
-            },
-            body:JSON.stringify({
-                email,oldpassword,newpassword
-            })
-        }).then((res)=>res.json()).then((data)=>console.log(data))
-    }
+  }, []);
 
-    return (
-        <>
-        <Container size={800} my={80} >
-         <Title
-              align="center"
-              sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}`, fontWeight: 900 })}
-            >
-              Change Password
-            </Title>
+  async function changepassword() {
+    await fetch(`${BACK_END_URL}/changepassword`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        oldpassword,
+        newpassword,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }
 
-            <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-              {/* {invalid && <Text color="red" align="center">Invalid credentials</Text>} */}
-              {/* {serverError && <Text color="red" align="center">Server error</Text>} */}
-              <TextInput 
-                label="Old Password" 
-                name="email" 
-                value={oldpassword}
-                onChange={(e) => setoldpass(e.target.value)} 
-                placeholder="Old Password" 
-                size="md"
-                required 
-              />
-              <PasswordInput 
-                label="New Password" 
-                value={newpassword}
-                name="password" 
-                onChange={(e) => setnewpass(e.target.value)} 
-                placeholder="New password" 
-                size="md"
-                required mt="md" 
-              />
-              <br />
-              {/* <Checkbox
+  return (
+    <>
+      <Container id="passwordchange" size={800} my={80}>
+        <Title
+          align="center"
+          sx={(theme) => ({
+            fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+            fontWeight: 900,
+          })}
+        >
+          Change Password
+        </Title>
+
+        <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+          {/* {invalid && <Text color="red" align="center">Invalid credentials</Text>} */}
+          {/* {serverError && <Text color="red" align="center">Server error</Text>} */}
+          <TextInput
+            label="Old Password"
+            name="email"
+            value={oldpassword}
+            onChange={(e) => setoldpass(e.target.value)}
+            placeholder="Old Password"
+            size="md"
+            required
+          />
+          <PasswordInput
+            label="New Password"
+            value={newpassword}
+            name="password"
+            onChange={(e) => setnewpass(e.target.value)}
+            placeholder="New password"
+            size="md"
+            required
+            mt="md"
+          />
+          <br />
+          {/* <Checkbox
                 label="Keep me logged in"
                 name="keep-logged-in"
                 // onChange={(e) => setRemember(e.target.checked)}
               /> */}
-              <Group position="apart" mt="lg">
-      
-                {/* <Anchor component="button" size="sm">
+          <Group position="apart" mt="lg">
+            {/* <Anchor component="button" size="sm">
                   Forgot password?
                 </Anchor> */}
-              </Group>
-              <Button fullWidth mt="xl" size="md"
-               onClick={changepassword} 
-               color="indigo">
-                Change Password
-              </Button>
-            </Paper>
+          </Group>
+          <Button
+            fullWidth
+            mt="xl"
+            size="md"
+            onClick={changepassword}
+            color="indigo"
+          >
+            Change Password
+          </Button>
+        </Paper>
+      </Container>
 
-        </Container>
+      <Container id="voiceregistration" size={800} my={80}>
+        <Title
+          align="center"
+          sx={(theme) => ({
+            fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+            fontWeight: 900,
+          })}
+        >
+          Voice Registration
+        </Title>
 
-        <Container size={800} my={80} >
-         <Title
-              align="center"
-              sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}`, fontWeight: 900 })}
-            >
-              Voice Registration
-            </Title>
-
-            <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-              {/* {invalid && <Text color="red" align="center">Invalid credentials</Text>} */}
-              {/* {serverError && <Text color="red" align="center">Server error</Text>} */}
-              {/* <TextInput 
-                label="Old Password" 
-                name="email" 
-                // value={email}
-                // onChange={(e) => setEmail(e.target.value)} 
-                placeholder="Old Password" 
-                size="md"
-                required 
-              /> */}
-              Press the start button and repeat the sentence into your microphone
-              
-              <br />
-              <div style={{backgroundColor:"wheat",marginTop:"20px",padding:"10px"}}>
-              I enjoy listening to music in my free time.What is the weather like today?
-              </div>
-            
-              <Group position="apart" mt="lg">
-      
-                {/* <Anchor component="button" size="sm">
-                  Forgot password?
-                </Anchor> */}
-              </Group>
-              <Button fullWidth mt="xl" size="md"
-            //    onClick={loginHandler} 
-               color="indigo">
-                Start
-              </Button>
-            </Paper>
-
-        </Container>
-        </>
-    )
+        <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+          Press the start button and repeat the sentence into your microphone
+          <br />
+          <div
+            style={{
+              backgroundColor: "wheat",
+              marginTop: "20px",
+              padding: "10px",
+            }}
+          >
+            I enjoy listening to music in my free time.What is the weather like
+            today?
+          </div>
+          <Group position="apart" mt="lg">
+          </Group>
+          <Button
+            fullWidth
+            mt="xl"
+            size="md"
+               onClick={getVoice}
+            color="indigo"
+          >
+            Start
+          </Button>
+        </Paper>
+      </Container>
+    </>
+  );
 }
--- src/pages/login.js
+++ src/pages/login.js
@@ -9,123 +9,149 @@ import {
   Container,
   Group,
   Button,
-} from '@mantine/core';
-import Link from 'next/link';
-import { useState } from 'react';
-import { useRouter } from 'next/router'
-import { Stepper } from '@mantine/core';
+} from "@mantine/core";
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { Stepper } from "@mantine/core";
 
 export default function AuthenticationTitle() {
   const [active, setActive] = useState(0);
-  const nextStep = () => setActive((current) => (current < 3 ? current + 1 : current));
-  const prevStep = () => setActive((current) => (current > 0 && localStorage.getItem('token') == undefined  ? current - 1 : current));
-  const router = useRouter()
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [remember, setRemember] = useState(false)
-  const [invalid, setInvalid] = useState(false)
-  const [serverError, setServerError] = useState(false)
-  const BACK_END_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:4000" ;
+  const nextStep = () =>
+    setActive((current) => (current < 3 ? current + 1 : current));
+  const prevStep = () =>
+    setActive((current) =>
+      current > 0 && localStorage.getItem("token") == undefined
+        ? current - 1
+        : current
+    );
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [remember, setRemember] = useState(false);
+  const [invalid, setInvalid] = useState(false);
+  const [serverError, setServerError] = useState(false);
+  const BACK_END_URL =
+    process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:4000";
   const loginHandler = async () => {
     fetch(`${BACK_END_URL}/login`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ email, password, remember })
+      body: JSON.stringify({ email, password, remember }),
     })
-    .then(res => res.json())
-    .then(data => {
-      console.log(data)
-      if(data.message === "Method not allowed"){
-        console.log("Method not allowed")
-        setServerError(true)
-        return
-      }
-      if(data.message === "Invalid credentials"){
-        setInvalid(true)
-        return
-      }
-      setInvalid(false)
-      setServerError(false)
-      if (data.user.token) {
-        localStorage.setItem('token', data.user.token)
-        localStorage.setItem('email', data.user.email)
-        localStorage.setItem('role', data.user.role)
-        if(data.message === "Voice rec and password needed")
-        {
-          router.push('/advance_register')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.message === "Method not allowed") {
+          console.log("Method not allowed");
+          setServerError(true);
+          return;
         }
-        else if(data.message === "Password change needed")
-        {
-          router.push(`/advance_register/${1}`)
+        if (data.message === "Invalid credentials") {
+          setInvalid(true);
+          return;
         }
-        else if(data.message === "Voice rec and password needed"){
-          router.push(`/advance_register/${2}`)
+        setInvalid(false);
+        setServerError(false);
+        if (data.user.token) {
+          localStorage.setItem("token", data.user.token);
+          localStorage.setItem("email", data.user.email);
+          localStorage.setItem("role", data.user.role);
+          if (data.message === "Voice rec and password needed") {
+            router.push({
+              pathname:"/advance_register",
+              query:{id:"0"}
+            });
+          } else if (data.message === "Password change needed") {
+            router.push({
+              pathname: "/advance_register",
+              query: { id: "1" },
+            });
+          } else if (data.message === "Voice rec needed") {
+            router.push({
+              pathname: "/advance_register",
+              query: {id:"2"}
+            });
+          } else {
+            router.push("/");
+          }
         }
-        else{
-        router.push('/')
-        }
-      }
-    }).catch(err => {
-      console.log(err)
-    })  
-  }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
-    <Container size={800} my={80} >
-              <Title
-              align="center"
-              sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}`, fontWeight: 900 })}
-            >
-              Salesine | Welcome back!
-            </Title>
-            <Text color="dimmed" size="sm" align="center" mt={5}>
-              Do not have an account yet?{' '}
-              <Anchor size="sm" component="button">
-                <Link href='/register'>
-                  Create account
-                </Link>
-                
-              </Anchor>
-            </Text>
+    <Container size={800} my={80}>
+      <Title
+        align="center"
+        sx={(theme) => ({
+          fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+          fontWeight: 900,
+        })}
+      >
+        Salesine | Welcome back!
+      </Title>
+      <Text color="dimmed" size="sm" align="center" mt={5}>
+        Do not have an account yet?{" "}
+        <Anchor size="sm" component="button">
+          <Link href="/register">Create account</Link>
+        </Anchor>
+      </Text>
 
-            <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-              {invalid && <Text color="red" align="center">Invalid credentials</Text>}
-              {serverError && <Text color="red" align="center">Server error</Text>}
-              <TextInput 
-                label="Email" 
-                name="email" 
-                value={email}
-                onChange={(e) => setEmail(e.target.value)} 
-                placeholder="you@mantine.dev" 
-                size="md"
-                required 
-              />
-              <PasswordInput 
-                label="Password" 
-                value={password}
-                name="password" 
-                onChange={(e) => setPassword(e.target.value)} 
-                placeholder="Your password" 
-                size="md"
-                required mt="md" 
-              />
-              <br />
-              <Checkbox
-                label="Keep me logged in"
-                name="keep-logged-in"
-                onChange={(e) => setRemember(e.target.checked)}
-              />
-              <Group position="apart" mt="lg">
-      
-                <Anchor component="button" size="sm">
-                  Forgot password?
-                </Anchor>
-              </Group>
-              <Button fullWidth mt="xl" size="md" onClick={loginHandler} color="indigo">
-                Sign in
-              </Button>
-            </Paper>
+      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+        {invalid && (
+          <Text color="red" align="center">
+            Invalid credentials
+          </Text>
+        )}
+        {serverError && (
+          <Text color="red" align="center">
+            Server error
+          </Text>
+        )}
+        <TextInput
+          label="Email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="you@mantine.dev"
+          size="md"
+          required
+        />
+        <PasswordInput
+          label="Password"
+          value={password}
+          name="password"
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Your password"
+          size="md"
+          required
+          mt="md"
+        />
+        <br />
+        <Checkbox
+          label="Keep me logged in"
+          name="keep-logged-in"
+          onChange={(e) => setRemember(e.target.checked)}
+        />
+        <Group position="apart" mt="lg">
+          <Anchor component="button" size="sm">
+            Forgot password?
+          </Anchor>
+        </Group>
+        <Button
+          fullWidth
+          mt="xl"
+          size="md"
+          onClick={loginHandler}
+          color="indigo"
+        >
+          Sign in
+        </Button>
+      </Paper>
     </Container>
   );
 }
