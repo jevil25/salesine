3b9e5fcaa5fbad2168f61cf1dc713835a0e0b348
AaronNazareth <4nm20cs005@nmamit.in>
2023-07-13 17:25:55 +0530

    team Dashboard activity added

--- src/components/TeamActivity.jsx
+++ src/components/TeamActivity.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import styles from '../styles/TeamActivity.module.css';
 
-const TeamsActivity = () => {
+const TeamsActivity = ({ team,calls }) => {
   const [activityName, setActivityName] = useState({
     callDuration: true,
     weeklyVolume: false,
@@ -9,6 +9,7 @@ const TeamsActivity = () => {
     totalVolume: false,
     totalDuration: false,
   });
+
 
   return (
     <div className={styles.activityWrapper}>
@@ -89,21 +90,97 @@ const TeamsActivity = () => {
           <div className={styles.activityWrapperCallDuration}>
             <div className={styles.callDurationInfo}>
               <div className={styles.callDurationInfoHead}>Call Duration</div>
-              <div className={styles.callDurationInfoSub}>Based on 625 calls</div>
+              <div className={styles.callDurationInfoSub}>Based on {calls.length} calls</div>
             </div>
             <div className={styles.callDurationGraph}>
               <div className={styles.graphLabel}>Team Members</div>
               <div className={styles.barGraph}>
-                <div className={styles.graph}>
-                  <div className={styles.graphName}>Name</div>
-                  <div className={`${styles.graphData} ${styles.width50}`}></div>
-                </div>
-                {/* Repeat the same pattern for the remaining graphs */}
+                {team.map((member) => (
+                  <div className={styles.graph}>
+                    <div className={styles.graphName}>{member.name}</div>
+                    <div className={`${styles.graphData}`} style={{"width":"50px"}}></div>50%
+                  </div>
+                ))}
               </div>
             </div>
           </div>
         )}
-        {/* Repeat the same pattern for the remaining activity panels */}
+        {activityName.weeklyVolume && (
+          <div className={styles.activityWrapperCallDuration}>
+            <div className={styles.callDurationInfo}>
+              <div className={styles.callDurationInfoHead}>Weekly Call Volume</div>
+              <div className={styles.callDurationInfoSub}>Based on {calls.length} calls</div>
+            </div>
+            <div className={styles.callDurationGraph}>
+              <div className={styles.graphLabel}>Team Members</div>
+              <div className={styles.barGraph}>
+                {team.map((member) => (
+                  <div className={styles.graph}>
+                    <div className={styles.graphName}>{member.name}</div>
+                    <div className={`${styles.graphData}`} style={{"width":"50px"}}></div>50%
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        )}
+        {activityName.weeklyDuration && (
+          <div className={styles.activityWrapperCallDuration}>
+            <div className={styles.callDurationInfo}>
+              <div className={styles.callDurationInfoHead}>Weekly Call Duration</div>
+              <div className={styles.callDurationInfoSub}>Based on {calls.length} calls</div>
+            </div>
+            <div className={styles.callDurationGraph}>
+              <div className={styles.graphLabel}>Team Members</div>
+              <div className={styles.barGraph}>
+                {team.map((member) => (
+                  <div className={styles.graph}>
+                    <div className={styles.graphName}>{member.name}</div>
+                    <div className={`${styles.graphData}`} style={{"width":"50px"}}></div>50%
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        )}
+        {activityName.totalVolume && (
+          <div className={styles.activityWrapperCallDuration}>
+            <div className={styles.callDurationInfo}>
+              <div className={styles.callDurationInfoHead}>Total Call Volume</div>
+              <div className={styles.callDurationInfoSub}>Based on {calls.length} calls</div>
+            </div>
+            <div className={styles.callDurationGraph}>
+              <div className={styles.graphLabel}>Team Members</div>
+              <div className={styles.barGraph}>
+                {team.map((member) => (
+                  <div className={styles.graph}>
+                    <div className={styles.graphName}>{member.name}</div>
+                    <div className={`${styles.graphData}`} style={{"width":"50px"}}></div>50%
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        )}
+        {activityName.totalDuration && (
+          <div className={styles.activityWrapperCallDuration}>
+            <div className={styles.callDurationInfo}>
+              <div className={styles.callDurationInfoHead}>Total Call Duration</div>
+              <div className={styles.callDurationInfoSub}>Based on {calls.length} calls</div>
+            </div>
+            <div className={styles.callDurationGraph}>
+              <div className={styles.graphLabel}>Team Members</div>
+              <div className={styles.barGraph}>
+                {team.map((member) => (
+                  <div className={styles.graph}>
+                    <div className={styles.graphName}>{member.name}</div>
+                    <div className={`${styles.graphData}`} style={{"width":"50px"}}></div>50%
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
