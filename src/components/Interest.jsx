c1ac3638719498958f4757ae5fd2f50e64f90e1d
AaronNazareth <4nm20cs005@nmamit.in>
2023-07-31 22:01:32 +0530

    recordings page crash fix

--- src/components/Interest.jsx
+++ src/components/Interest.jsx
@@ -8,6 +8,35 @@ const Interest = ({ trackers,meetHost }) => {
         name: '',
         role: ''
     })
+    if (trackers === undefined || trackers === null) {
+        return <div className={styles.interestWrapper}>
+            <div className={styles.interestHeading}>
+                <p>Point of Interest</p>
+            </div>
+            <div className={styles.interestSearch}>
+                <input type="text" placeholder='Search for Keywords.' />
+            </div>
+            <div className={styles.actionItems}>
+                <div className={styles.actionItemHeading}>TRACKERS</div>
+                <div className={styles.trackersItem}>
+                    <div className={styles.trackerItemSubHeading}>
+                        <div className={styles.trackItem}>
+                            <div className={styles.itemName}>No trackers found, Our Model is processing your video</div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.statsContainer} style={{ borderBottom: "0px", background: '#F4F8FF', borderRadius: '5px', marginBottom: '20px', alignItems: 'center', paddingTop: '10px', gap: '10px',marginTop:'5px' }}>
+                <div className={styles.call_detailshead}>Call Details:</div>
+                    <div className={styles.call_deatilsbody}>
+                        <div className={styles.call_detailshead}>{userDetails.name}, {userDetails.role}</div>
+                        <ul>
+                            <li>Primary Rep</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    }
     const [meetHostId, setMeetHostId] = useState(meetHost)
     //get keys from trackers object
     useEffect(() => {
